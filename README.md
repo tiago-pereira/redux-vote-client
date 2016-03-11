53d96f2ae2ef83ab0960d1f5fb2b729065366817
tiago-pereira <tiagops.prog@gmail.com>
2016-03-10 22:24:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# redux-learn
+Rep to save redux react es6 tries
+
+
+http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html#what-you-will-need
+
+
+- Last step 
+npm install --save react-router@2.0.0
---
+++ src/components/Voting.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  render: function() {
+    return <div>
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
+    </div>;
+  }
+});
